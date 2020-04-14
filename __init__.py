aed0f88a55f46fa0ce4928914c2b9224a75924c9
aalmethen <ahmedalmethen@gmail.com>
2020-04-14 20:27:03 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -34,7 +34,7 @@ class N2(db.Model):
     __tablename__ = 'names'
    
 
-    name = db.Column(db.Text,primary_key = True))
+    name = db.Column(db.Text,primary_key = True)
     
 
     def __init__(self,name):
